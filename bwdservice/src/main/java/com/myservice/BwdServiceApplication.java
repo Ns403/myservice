70833a543f748010124fbc32b18b449ce263ba80
苏宁生 <ns@403.com>
2019-05-23 17:55:59 +0800

    首次提交！springboot环境！

---
+++ bwdservice/src/main/java/com/myservice/BwdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwdServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BwdServiceApplication.class, args);
+    }
+
+}
